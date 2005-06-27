c4e60db2dd195381bf31cc588d184fa9842e2d98
Lester Caine <lester@lsces.co.uk>
2005-06-27 15:10:29 +0000

    Adding DocBlock elements to improve phpDocumentor output

--- LibertyGatekeeper.php
+++ LibertyGatekeeper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_gatekeeper/LibertyGatekeeper.php,v 1.1.1.1.2.3 2005/06/27 10:55:46 lsces Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_gatekeeper/LibertyGatekeeper.php,v 1.1.1.1.2.4 2005/06/27 15:10:29 lsces Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: LibertyGatekeeper.php,v 1.1.1.1.2.3 2005/06/27 10:55:46 lsces Exp $
+ * $Id: LibertyGatekeeper.php,v 1.1.1.1.2.4 2005/06/27 15:10:29 lsces Exp $
  * @package gatekeeper
  */
 
@@ -21,13 +21,14 @@ require_once( LIBERTY_PKG_PATH.'LibertyBase.php' );
  * Gatekeeper class to illustrate best practices when creating a new bitweaver package that
  * builds on core bitweaver functionality, such as the Liberty CMS engine
  *
- * @package LibertyGatekeeper
+ * @package gatekeeper
+ * @subpackage LibertyGatekeeper
  *
  * created 2004/8/15
  *
  * @author spider <spider@steelsun.com>
  *
- * @version $Revision: 1.1.1.1.2.3 $ $Date: 2005/06/27 10:55:46 $ $Author: lsces $
+ * @version $Revision: 1.1.1.1.2.4 $ $Date: 2005/06/27 15:10:29 $ $Author: lsces $
  */
 class LibertyGatekeeper extends LibertyBase {
     /**
