96ff3c967e6af769fdac9591710db73131a0ef94
Christian Fowler <spider@viovio.com>
2005-07-27 17:51:11 +0000

    fix page title

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 // | Authors: spider <spider@steelsun.com>
 // +----------------------------------------------------------------------+
 //
-// $Id: index.php,v 1.1.1.1.2.1 2005/07/26 15:50:06 drewslater Exp $
+// $Id: index.php,v 1.1.1.1.2.2 2005/07/27 17:51:11 spiderr Exp $
 
 require_once( '../bit_setup_inc.php' );
 
@@ -22,6 +22,6 @@ require_once( GATEKEEPER_PKG_PATH.'LibertyGatekeeper.php' );
 $lists = $gGatekeeper->getSecurityList();
 $gBitSmarty->assign_by_ref( 'securities', $lists );
 
-$gBitSystem->display( 'bitpackage:gatekeeper/list_security.tpl', 'Upload Images' );
+$gBitSystem->display( 'bitpackage:gatekeeper/list_security.tpl', 'Security Lists' );
 
 ?>
