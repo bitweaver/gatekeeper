e497bd6e16e192a242ee5adbc73cbdc2571a549c
Christian Fowler <spider@viovio.com>
2005-08-14 21:29:13 +0000

    fix stabs at the Liberty Services concept to remove gatekeeper specifics from the packages

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,23 +3,12 @@ global $gBitSystem, $gBitSmarty;
 $gBitSystem->registerPackage( 'gatekeeper', dirname( __FILE__).'/' );
 
 require_once( GATEKEEPER_PKG_PATH.'LibertyGatekeeper.php' );
-//if( $gBitSystem->isPackageActive( 'gatekeeper' ) ) {
 
-	// Gallery is visible to everyone with bit_p_view_fisheye permissions
-//	define( 'GATEKEEPER_ACCESS_PUBLIC', 100);
-	// The content will no longer show up in lists or searches
-//	define( 'GATEKEEPER_ACCESS_PROTECTED_HIDDEN' , 200);	
-	// Users must correctly answer a question to be able to view gallery
-//	define( 'GATEKEEPER_ACCESS_PROTECTED', 300);		
-	// Completely private gallery. Viewable only by the creator and members of an optional assigned group
-//	define( 'GATEKEEPER_ACCESS_PRIVATE', 500);			
+if( $gBitSystem->isPackageActive( 'gatekeeper' ) ) {
+	$gLibertySystem->registerService( LIBERTY_SERVICE_ACCESS_CONTROL, GATEKEEPER_PKG_NAME, array(
+		'edit_choose_tpl' => 'bitpackage:gatekeeper/choose_security.tpl',
+	) );
 
-//	$gBitSmarty->assign('GATEKEEPER_ACCESS_PUBLIC', GATEKEEPER_ACCESS_PUBLIC);
-//	$gBitSmarty->assign('GATEKEEPER_ACCESS_PROTECTED', GATEKEEPER_ACCESS_PROTECTED);
-//	$gBitSmarty->assign('GATEKEEPER_ACCESS_PROTECTED_HIDDEN', GATEKEEPER_ACCESS_PROTECTED_HIDDEN);
-//	$gBitSmarty->assign('GATEKEEPER_ACCESS_PRIVATE', GATEKEEPER_ACCESS_PRIVATE);
-
-//	define ( 'GATEKEEPER_DEFAULT_ACCESS_LEVEL', GATEKEEPER_ACCESS_PUBLIC);
-//	define ( 'GATEKEEPER_DEFAULT_ACCESS_QUESTION', 'Enter the password to view this gallery');
+}
 
 ?>
