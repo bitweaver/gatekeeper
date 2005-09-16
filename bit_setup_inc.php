9149bbae0b81e4ea0497636caa3753ec78ae2d9f
Max Kremmel <xing@synapse.plus.com>
2005-09-16 17:45:24 +0000

    check for $gLibertyService availability - needed during install

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -4,13 +4,15 @@ $gBitSystem->registerPackage( 'gatekeeper', dirname( __FILE__).'/' );
 
 require_once( GATEKEEPER_PKG_PATH.'LibertyGatekeeper.php' );
 
-$gLibertySystem->registerService( LIBERTY_SERVICE_ACCESS_CONTROL, GATEKEEPER_PKG_NAME, array(
-	'content_display_function' => 'gatekeeper_content_display',
-	'content_edit_function' => 'gatekeeper_content_edit',
-	'content_store_function' => 'gatekeeper_content_store',
-	'content_load_function' => 'gatekeeper_content_load',
-	'content_verify_access' => 'gatekeeper_content_verify_access',
-	'content_edit_tpl' => 'bitpackage:gatekeeper/choose_security.tpl',
-	'content_icon_tpl' => 'bitpackage:gatekeeper/gatekeeper_service_icon.tpl',
-) );
+if( !empty( $gLibertySystem ) ) {
+	$gLibertySystem->registerService( LIBERTY_SERVICE_ACCESS_CONTROL, GATEKEEPER_PKG_NAME, array(
+		'content_display_function' => 'gatekeeper_content_display',
+		'content_edit_function' => 'gatekeeper_content_edit',
+		'content_store_function' => 'gatekeeper_content_store',
+		'content_load_function' => 'gatekeeper_content_load',
+		'content_verify_access' => 'gatekeeper_content_verify_access',
+		'content_edit_tpl' => 'bitpackage:gatekeeper/choose_security.tpl',
+		'content_icon_tpl' => 'bitpackage:gatekeeper/gatekeeper_service_icon.tpl',
+	) );
+}
 ?>
