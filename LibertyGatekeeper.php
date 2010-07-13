aa85a1070fc68c44f2bc8ae967646dcf39281c59
spiderr <spider@viovio.com>
2010-07-12 23:08:10 -0400

    add pObject->isValid to gatekeeper_content_list to keep compatibility with old isContentType method

--- LibertyGatekeeper.php
+++ LibertyGatekeeper.php
@@ -300,7 +300,7 @@ function gatekeeper_content_list( $pObject, $pParamHash ) {
 */
 	global $gBitSystem, $gGatekeeper, $gBitUser;
 	
-	if( is_a( $pObject, 'FisheyeImage' ) ) {
+	if( is_a( $pObject, 'FisheyeImage' ) && $pObject->isValid() ) {
 		if( $gBitSystem->mDb->isAdvancedPostgresEnabled() ) {
 			$ret['where_sql'] = " AND (SELECT gks.`security_id` FROM connectby('fisheye_gallery_image_map', 'gallery_content_id', 'item_content_id', fi.`content_id`, 0, '/')  AS t(`cb_gallery_content_id` int, `cb_item_content_id` int, level int, branch text), `".BIT_DB_PREFIX."gatekeeper_security_map` cgm,  `".BIT_DB_PREFIX."gatekeeper_security` gks
 					  WHERE gks.`security_id`=cgm.`security_id` AND cgm.`content_id`=`cb_gallery_content_id` LIMIT 1) IS NULL";
